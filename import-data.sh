83813418da7e306520724abc3c7e43ee5033fb0d
Patrick Holloway <patrick@kinvey.com>
2018-12-21 13:54:23 -0500

    wip import data script using curl

---
+++ import-data.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+APPID=$2
+MASTERSECRET=$3
+
+TOKEN=openssl base64 -e <<< $(APPID):$(MASTERSECRET)
+
+for file in ./import-data/**.json
+do
+  for row in $(echo "$(cat $file)" | jq -r '.[] | @base64'); do
+    _jq() {
+      echo ${row} | base64 --decode | jq -r ${1}
+     }
+    curl -X POST -d @./import-data/$file -H "Authentication: Basic $(token)" -H "Content-Type: application/json" https://stg-us1-baas.kinvey.com/appdata/$(APPID)/$(COLLECTION_NAME)
+  done
+done
